892fe61f0870d87bc61ae4c02159db7b17636813
AMAN BHARTI <amanbharti0302@gmail.com>
2020-09-02 01:48:43 +0530

    completed checkout page after adding additem and remove item

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types';
-import {addItemToCart} from './cart.utils';
+import {addItemToCart,removeItemFromCart} from './cart.utils';
 
 const INITIAL_STATE ={
     hidden:true,
@@ -19,7 +19,16 @@ const cartReducer = (state=INITIAL_STATE,action)=>{
                 ...state,
                 cartItems: addItemToCart(state.cartItems,action.payload)              //here we will return cart items after upgrading it according to payload
             }
-
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return{
+                ...state,
+                cartItems:state.cartItems.filter(cartItem=>cartItem.id!==action.payload.id)
+            }
+        case CartActionTypes.REMOVE_ITEM:
+            return{
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems,action.payload)
+            }
     default:
         return state;
     }
