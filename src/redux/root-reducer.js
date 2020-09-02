53a99a5e26a1ad5506063f503405ed675fe2a321
AMAN BHARTI <amanbharti0302@gmail.com>
2020-09-02 23:25:05 +0530

    session storage implemented

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,13 +2,25 @@
 // here we use combineReducer from react library to combine them all together
 
 import {combineReducers} from 'redux';
+import {persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';  //local storage object on window
+
+
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
 
+const persistConfig = {
+    key:'root',
+    storage,
+    whitelist:['cart']
+}
 
-// remeber all state in reducers in a whole json state
-export default combineReducers({
+const rootReducer =combineReducers({
     user:userReducer,
     cart:cartReducer
-})
+});
+
+
+// remeber all state in reducers in a whole json state
+export default persistReducer(persistConfig,rootReducer);  //modified root reducer for storage
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import {createStore ,applyMiddleware} from 'redux';
+import {persistStore} from 'redux-persist';  //for session storage and local storage
 
 import logger from 'redux-logger';     //to monitor redux state
 
@@ -9,9 +10,9 @@ const middlewares = [logger];     //here we stored logger in array ths method is
 
 //in applyMiddleware we can send directly logger here as we have only one middleware
 //...middleware is used to destructure all middlewares
-const store = createStore(rootreducer,applyMiddleware(...middlewares));
+export const store = createStore(rootreducer,applyMiddleware(...middlewares));
 
-
+export const persistor = persistStore(store);
 
 // use this store in provider of index js as global state
-export default store;
+//export default {store,persistor};
